97fbdaaea7162e178e06fc1a4febdd1f2500d3fc
Hector Flores <hecto932@gmail.com>
2019-02-11 15:32:40 -0600

    Fixing body changes on css

--- src/job/components/job.js
+++ src/job/components/job.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import './job.scss';
 
 const Job = (props) => (
   <div className="container">
     { props.info !== null && 
       <div className="row">
-        <section className="Job">
-          <div className="card">
-            <div className="card-header">
-              {props.info.uuid} - {props.info.job_title}
+        <div className="col-md-12">
+          <section className="Job">
+            <div className="card">
+              <div className="card-header">
+              <a href="#" class="badge badge-success">{props.info.job_uuid}</a> - {props.info.job_title}
+              </div>
+              <div className="card-body">
+                <ul>
+                {
+                  props.info.skills.map(item => {
+                    return <li>{item.skill_name}</li>
+                  })
+                }
+              </ul>
+              </div>
             </div>
-            <div className="card-body">
-              <ul>
-              {
-                props.info.skills.map(item => {
-                  return <li>{item.skill_name}</li>
-                })
-              }
-            </ul>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     }
   </div>
